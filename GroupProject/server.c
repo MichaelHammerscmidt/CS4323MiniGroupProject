68f214587c6fc645c17c3c9ef94966662325652c
MichaelHammerscmidt <mihamme@okstate.edu>
2022-09-25 16:18:17 -0500

    Attempted to fix compilation errors in server.c file

--- GroupProject/server.c
+++ GroupProject/server.c
@@ -19,6 +19,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/wait.h>
 
 // Rebecca's Code ----------------------------------------------------------------
 
@@ -99,8 +100,9 @@ bool readFile(char fileName[], DataTable *ptable)
     char errorMsg[] = "Error. File Null";
 
     printf("Read file: %s", &fileName[0]);
-    fopen_s(&file, fileName, "r");
-    if (file == NULL)
+    // fopen_s(&file, fileName, "r");
+    fopen(fileName, "r");
+    if (&fileName == NULL)
     {
         printf("%s", errorMsg);
         return false;
@@ -216,6 +218,102 @@ struct my_msg
 
 int processes[N_PROCESSES];
 
+char ***readFileByUniqueValue(char *filename, char *column, char *uniqueValue, int *row_, int *col_)
+{
+
+    int nRows = 705;
+    int nCols = 5;
+    int nValues = 300;
+
+    if (strcmp(filename, "amazonBestsellers.txt") == 0)
+    {
+        nRows = 555;
+        nCols = 6;
+    }
+
+    char ***recordArray = (char ***)malloc(nRows * sizeof(char **));
+
+    for (int i = 0; i < nRows; i++)
+    {
+        recordArray[i] = (char **)malloc(nCols * sizeof(char *));
+
+        for (int j = 0; j < nCols; j++)
+        {
+            recordArray[i][j] = (char *)malloc(nValues * sizeof(char));
+        }
+    }
+
+    FILE *inFile = fopen(filename, "r");
+    if (!inFile)
+    {
+        perror("fopen for reading");
+        exit(EXIT_FAILURE);
+    }
+
+    char *line = (char *)malloc(sizeof(char));
+    int row = 0;
+    int col = 0;
+
+    int columnIndex = 0;
+
+    while (fscanf(inFile, "%[^\n] ", line) != EOF)
+    {
+
+        char *pt = strtok(line, ",");
+
+        col = 0;
+
+        int i = 0;
+        while (pt != NULL)
+        {
+
+            if (row == 0 && strcmp(pt, column) == 0)
+            {
+                columnIndex = i;
+            }
+
+            if (columnIndex != 0 && columnIndex == i && strcmp(pt, uniqueValue) != 0 && strcmp(pt, column) != 0)
+            {
+                row--;
+                // printf("row = %d, col = %d\n", row, col);
+                break;
+            }
+
+            if (columnIndex == 0 || columnIndex != i)
+            {
+
+                for (int k = 0; k < strlen(pt); k++)
+                {
+                    recordArray[row][col][k] = pt[k];
+                }
+
+                //                 printf("recordArray[%d][%d] = %s\n", row, col, recordArray[row][col]);
+                //                 printf("recordArray[%d][%d] = %s\n", row, col, pt);
+
+                col++;
+            }
+
+            pt = strtok(NULL, ",");
+            i++;
+        }
+
+        row++;
+    }
+
+    //     for (int i = 0; i < row; i++) {
+    //         for (int j = 0; j < nCols; j++) {
+    //             printf("recordArray[%d][%d] = %s\n", i, j, recordArray[i][j]);
+    //         }
+    //         printf("\n");
+    //     }
+
+    //     fclose(inFile);
+
+    *row_ = row;
+    *col_ = nCols;
+    return recordArray;
+}
+
 void createProcesses(int nProcess, char *filename, char *column, char uniques[][15])
 {
 
@@ -329,102 +427,6 @@ int col = 0;
 //     // char uniqueValues[2][20] = {"In stock", "Out of stock"};
 //     // createProcesses(2, uniqueValues, recordArray, column, row, col);
 // }
-
-char ***readFileByUniqueValue(char *filename, char *column, char *uniqueValue, int *row_, int *col_)
-{
-
-    int nRows = 705;
-    int nCols = 5;
-    int nValues = 300;
-
-    if (strcmp(filename, "amazonBestsellers.txt") == 0)
-    {
-        nRows = 555;
-        nCols = 6;
-    }
-
-    char ***recordArray = (char ***)malloc(nRows * sizeof(char **));
-
-    for (int i = 0; i < nRows; i++)
-    {
-        recordArray[i] = (char **)malloc(nCols * sizeof(char *));
-
-        for (int j = 0; j < nCols; j++)
-        {
-            recordArray[i][j] = (char *)malloc(nValues * sizeof(char));
-        }
-    }
-
-    FILE *inFile = fopen(filename, "r");
-    if (!inFile)
-    {
-        perror("fopen for reading");
-        exit(EXIT_FAILURE);
-    }
-
-    char *line = (char *)malloc(sizeof(char));
-    int row = 0;
-    int col = 0;
-
-    int columnIndex = 0;
-
-    while (fscanf(inFile, "%[^\n] ", line) != EOF)
-    {
-
-        char *pt = strtok(line, ",");
-
-        col = 0;
-
-        int i = 0;
-        while (pt != NULL)
-        {
-
-            if (row == 0 && strcmp(pt, column) == 0)
-            {
-                columnIndex = i;
-            }
-
-            if (columnIndex != 0 && columnIndex == i && strcmp(pt, uniqueValue) != 0 && strcmp(pt, column) != 0)
-            {
-                row--;
-                // printf("row = %d, col = %d\n", row, col);
-                break;
-            }
-
-            if (columnIndex == 0 || columnIndex != i)
-            {
-
-                for (int k = 0; k < strlen(pt); k++)
-                {
-                    recordArray[row][col][k] = pt[k];
-                }
-
-                //                 printf("recordArray[%d][%d] = %s\n", row, col, recordArray[row][col]);
-                //                 printf("recordArray[%d][%d] = %s\n", row, col, pt);
-
-                col++;
-            }
-
-            pt = strtok(NULL, ",");
-            i++;
-        }
-
-        row++;
-    }
-
-    //     for (int i = 0; i < row; i++) {
-    //         for (int j = 0; j < nCols; j++) {
-    //             printf("recordArray[%d][%d] = %s\n", i, j, recordArray[i][j]);
-    //         }
-    //         printf("\n");
-    //     }
-
-    //     fclose(inFile);
-
-    *row_ = row;
-    *col_ = nCols;
-    return recordArray;
-}
 
 // Cody's server ---------------------------------------------------------------
 
