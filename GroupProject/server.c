b85fd2d8d144a4cc2285f5541e51a685e2f1d9be
MichaelHammerscmidt <mihamme@okstate.edu>
2022-09-25 21:12:35 -0500

    Commented out Jacobs code for testing.

--- GroupProject/server.c
+++ GroupProject/server.c
@@ -210,43 +210,43 @@ bool readFile(char fileName[], DataTable *ptable)
 // Jacob's Code   ----------------------------------------------------------------
 
 
-int saveFile(DataTable *ptable, DataColumn *pcolumn)
-{
+// int saveFile(DataTable *ptable, DataColumn *pcolumn)
+// {
 
 
-int i, j, k; //initializing variables for later for loops
+// int i, j, k; //initializing variables for later for loops
 
-int n = gets(DataTable.numCols); //initialize n for the number of columns in the table
-int z = gets(DataTable.numRows); //initialize z for the number of rows in the table
+// int n = gets(DataTable.numCols); //initialize n for the number of columns in the table
+// int z = gets(DataTable.numRows); //initialize z for the number of rows in the table
 
-FILE *output; //creating file
+// FILE *output; //creating file
 
-output = fopen ("output.txt", "w"); //open the file for writing
-
-
-
-for(k = 0; k < n, k++) //loop for adding the data headers to the file
-{
-	gets((ptable.colHeaders);
-	printf("\t\t");
+// output = fopen ("output.txt", "w"); //open the file for writing
 
 
 
-}
+// for(k = 0; k < n, k++) //loop for adding the data headers to the file
+// {
+// 	gets((ptable.colHeaders);
+// 	printf("\t\t");
 
-for(j = 0; j < z; j++) //loop for adding the rest of the data to the file. This loop is for the rows
-{
-	for(i = 0; i < n; i++) //this loop is for the columns
-	{
-		gets(*pcolumn.columns);
-		printf("\t\t"); //formatting
+
+
+// }
+
+// for(j = 0; j < z; j++) //loop for adding the rest of the data to the file. This loop is for the rows
+// {
+// 	for(i = 0; i < n; i++) //this loop is for the columns
+// 	{
+// 		gets(*pcolumn.columns);
+// 		printf("\t\t"); //formatting
 
 	
-	}
+// 	}
 
-	printf("\n"); //formatting
+// 	printf("\n"); //formatting
 
-}
+// }
 
 
 /*
