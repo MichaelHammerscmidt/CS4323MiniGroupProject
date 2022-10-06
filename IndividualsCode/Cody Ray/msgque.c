0f79609471737b507c3a8fcc8612673faf2424dd
CodyRay44 <codyaray44@gmail.com>
2022-10-05 20:58:40 -0500

    zzz

--- IndividualsCode/Cody Ray/msgque.c
+++ IndividualsCode/Cody Ray/msgque.c
@@ -4,41 +4,48 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct msgQue{
     long msgType;
-    char msgText[256];
+    char msgText[1028];
 }message;
 
 int main(){
     int numOfProcesses = 3;
     int currentProcess = 0;
-    key_t processKey;
-    pid_t parentID;
-    pid_t childID;
+    int childID;
     int msgID;
+    int msgCheck;
 
-    strcpy(message.msgText,"hello");
-
-    parentID = getpid();
-
-    while(currentProcess < numOfProcesses){
+    //while(currentProcess < numOfProcesses){
         childID = fork();
-        processKey = childID;
-        msgID = msgget(processKey, 0666 | IPC_CREAT);
+        msgID = msgget(childID, 0666 | IPC_CREAT);
 
-        if(parentID == getpid()){
-            printf("current PID = %d\n", getpid());
-            msgsnd(msgID, &message, sizeof(message), 0);
-            wait(NULL);
+        printf("CURRENT PROCESS: %d\n",currentProcess);
+
+        if(childID < 0){
+            printf("Error forking");
+        }else if(childID == 0){
+            printf("Entering child process PID: %d\n", childID);
+            msgCheck = msgrcv(msgID, &message, sizeof(message),1,IPC_NOWAIT);
+            sleep(20);
+            if(msgCheck == -1){
+                printf("Error receiving message\n");
+            }
+            printf("%s\n",message.msgText);
+            msgctl(msgID, IPC_RMID, NULL);
+        }else{
+            printf("Entering parent process PID: %d\n", childID);
+            strcpy(message.msgText,"hello");
+            sleep(5);
+            msgCheck = msgsnd(msgID, &message, sizeof(message), 0);
+            if(msgCheck == -1){
+                printf("Error sending message\n");
+            }
         }
-        if (childID != parentID) {
-            printf("%d received\n", currentProcess);
-            msgrcv(msgID, &message, sizeof(message),1,0);
-            printf("%s",message);
-        }
-        currentProcess++;
-    }
+        //currentProcess++;
+    //}
 
     msgctl(msgID, IPC_RMID, NULL);
     return 0;
