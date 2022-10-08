0d098f3104d21bd939c20fc8c267e0db8131919c
MichaelHammerscmidt <mihamme@okstate.edu>
2022-10-07 20:22:49 -0500

    Added loop for number of processes to msgQueue

--- IndividualsCode/Michael Hammerschmidt/msgQueue.c
+++ IndividualsCode/Michael Hammerschmidt/msgQueue.c
@@ -1,19 +1,4 @@
-// Online C compiler to run C program online
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -28,20 +13,20 @@ struct msgQue{
 };
 
 int main(){
-    //int numOfProcesses = 2;
-    //int currentProcess = 0;
+    int numOfProcesses = 3;
+    int currentProcess = 0;
     int msgID;
     int msgCheck;
-    //int running = 1;
+    int running = 1;
     struct msgQue message;
     
     strcpy(message.msgText,"Sending failed:(");
     
     int t;
 
-    //while(currentProcess < numOfProcesses && running == 1){
+    while(currentProcess < numOfProcesses && running == 1){
         msgID = msgget((key_t)t, 0666 | IPC_CREAT);
-        printf("msgID =  %d\n", msgID);
+        //printf("msgID =  %d\n", msgID);
         if(msgID == -1){
             printf("Error making message queue\n");
         }
@@ -61,37 +46,38 @@ int main(){
                 printf("Recieved message: %s\n",message.msgText);
                 if(strncmp(message.msgText, "Sending success!", 16) == 0){
                     printf("Success!\n");
-                    //running = 0;
+                    running = 0;
                     //break;
                 }
+                
+                //TODO: Christian's logic
                 
             }else{
                 //sleep(1);
                 printf("Entering parent process PID: %d\n", t);
                 message.msgType = 1;
                 strcpy(message.msgText,"Sending success!");
-                printf("Size of msgText: %ld\n", sizeof(message.msgText));
-                printf("Size of msgText 2: %ld\n", sizeof(struct msgQue) - sizeof(long int));
+                //printf("Size of msgText: %ld\n", sizeof(message.msgText));
+                //printf("Size of msgText 2: %ld\n", sizeof(struct msgQue) - sizeof(long int));
                 msgCheck = msgsnd(msgID, (void *)&message, sizeof(message.msgText), 0);
-                printf("msgType = %ld\n", message.msgType);
+                //printf("msgType = %ld\n", message.msgType);
                 if(msgCheck == -1){
                     printf("Error sending message\n");
                 }else{
                     //running = 0;
-                    printf("MsgCheck = %d\n", msgCheck);
+                    //printf("MsgCheck = %d\n", msgCheck);
                 }
                 printf("\nMessage sent\n");
             }
-        //currentProcess++;
-    //}
+        currentProcess++;
+        printf("Process %d finished\n", t);
+    }
     
     
     if(t == 0){
-        printf("Destroying msgqueueueueu\n");
         int var = msgctl(msgID, IPC_RMID, NULL);
-        printf("\n%d\n", var);
+        printf("\nDestoryed %d\n", var);
     }
-    printf("Process %d finished\n", t);
     return 0;
 }
 
