390c6a37783be6538340b5480d426c5175e40533
Chrisantus Eze <ezechris30@gmail.com>
2022-10-10 11:31:51 -0500

    remove irrelevant functions from the read_file.c and .h files

--- GroupProject/read_file.h
+++ GroupProject/read_file.h
@@ -7,15 +7,4 @@ struct uniqueRecordStruct readFile(char* filename);
 char** getUniqueValues(struct uniqueRecordStruct records, char* column, int* returned_size);
 struct uniqueRecordStruct getRecordsByUniqueValue(struct uniqueRecordStruct records, char* column, char* uniqueValue);
 
-/*
-void snd(struct processDataStruct data);
-struct processDataStruct rcv(char uniqueValue[20]);
-
-void msgsnd_(int msgid, struct processDataStruct data, int size, int pms);
-struct processDataStruct msgrcv_(int msgid, char uniqueValue[20], int size, int pms, int detr);
-
-void sendDataToParent(struct processDataStruct data);
-struct uniqueRecordStruct receiveDataFromProcess(char filename[20], char column[20], char uniqueValue[20]);
-
-*/
 #endif
